8978e956790b92ec1410d87f26328c1392492b66
maxlo <maxloginov2001@mail.ru>
2023-05-25 14:16:44 +0300

    commit project

---
+++ catalog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
