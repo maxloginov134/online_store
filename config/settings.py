a322e12490fe3b56edd414480a9f2e1131faebee
maxlo <maxloginov2001@mail.ru>
2023-06-01 18:19:11 +0300

    Добавления: Модели, Админка, Настроил подключение к БД

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_bd',
+        'USER': 'postgres',
+        'PASSWORD': '6218',
     }
 }
 
